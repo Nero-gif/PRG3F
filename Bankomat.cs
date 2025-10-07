9346f32d50fa8b820121845632b649849f268892
Jakub Lukšík <jakub.luksik@infis.cz>
2025-10-07 10:20:12 +0200

    idkužale

--- Bankomat.cs
+++ Bankomat.cs
@@ -5,34 +5,42 @@ public class Bankomat
     public static void Main1(String[] args)
     {
         int account = 0;
-        
-        //dodělat loop
-        
-        System.Console.WriteLine("Vyberte akci:\n 1 = Vypsat zůstatek na účtě\n 2 = Vložit peníze na účet\n 3 = Donate bance\n 4 = Free money\n 5 = Game mode\n 6 = Exit");
-        int input = int.Parse(Console.ReadLine());
-        Console.WriteLine(input);
-        if (input == 1)
+
+        bool someArgumentToEndTheWhile = true;
+        while (someArgumentToEndTheWhile)
         {
-            moneyBalance(account);
-        }else if (input == 2)
-        {
-            insertMoney(account);
-        }else if (input == 3)
-        {
-            donate(account);
-        }else if (input == 4)
-        {
-            freeMoney();
-        }else if (input == 5)
-        {
-            gameMode(account);
-        }else if (input == 6)
-        {
-            exit(account);
-        }
-        else
-        {
-            userIsIdiot(account);
+            System.Console.WriteLine(
+                "Vyberte akci:\n 1 = Vypsat zůstatek na účtě\n 2 = Vložit peníze na účet\n 3 = Donate bance\n 4 = Free money\n 5 = Game mode\n 6 = Exit");
+            int input = int.Parse(Console.ReadLine());
+            if (input == 1)
+            {
+                moneyBalance(account);
+            }
+            else if (input == 2)
+            {
+                account = insertMoney(account);
+            }
+            else if (input == 3)
+            {
+                account = donate(account);
+            }
+            else if (input == 4)
+            {
+                freeMoney();
+            }
+            else if (input == 5)
+            {
+                account = gameMode(account);
+            }
+            else if (input == 6)
+            {
+                account = exit(account);
+                someArgumentToEndTheWhile = false;
+            }
+            else
+            {
+                account = userIsIdiot(account);
+            }
         }
     }
 
@@ -52,10 +60,10 @@ public class Bankomat
         }
     }
 
-    public static void insertMoney(int account)
+    public static int insertMoney(int account)
     {
         Console.WriteLine("Zadajte částku ke vložení.");
-        int insertion = Console.Read();
+        int insertion = int.Parse(Console.ReadLine());
         if (insertion > 0)
         {
             account = account + insertion;
@@ -65,12 +73,14 @@ public class Bankomat
         {
             Console.WriteLine("Asi máš trochu problém. Pohodlně se usaď a počkej až ti přijdou fešáci v uniformách pomoct.");
         }
+        
+        return account;
     }
 
-    public static void donate(int account)
+    public static int donate(int account)
     {
         Console.WriteLine("Kolik zbytečných peněz na účtě máte?");
-        int donate = Console.Read();
+        int donate = int.Parse(Console.ReadLine());
         if (donate <= 0)
         {
             Console.WriteLine("Dobrej pokus, ale smůla.");
@@ -88,42 +98,57 @@ public class Bankomat
             account = account - donate;
             Console.WriteLine("Zůstatek na vašem účtu je " + account);
         }
+        
+        return account;
     }
 
     public static void freeMoney()
     {
-        Console.WriteLine("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡻⢀⠆⣤⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⢹⣴⢊⣀⣿⡏⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠙⡛⠦⢤⠟⣰⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡘⡄⠁⢀⠰⣸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡼⣝⣁⢲⡐⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⡿⡁⠚⢋⠭⢱⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⠀⠄⠀⠒⣹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠖⢀⣠⠀⢀⠗⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢃⠢⣄⣄⡂⠣⣜⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣸⣱⣠⣂⠩⢷⡸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣰⣴⣛⣔⣺⡇⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣯⢗⡻⠶⠽⢤⣿⢋⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠴⣖⡶⣶⣟⢣⡙⢋⡭⢎⣻⣽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⣠⣾⢏⣼⠏⣵⣿⣿⣣⠞⠦⡘⣐⢲⡏⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⣯⣳⡀⢀⣾⠟⣣⠾⣇⠊⡴⣿⡥⠒⡌⠐⢤⡘⣼⠁⣰⠋⣴⡾⣭⡲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⣠⢿⡿⠰⣿⣷⠿⣻⠩⢅⠋⠤⠁⣾⣯⡱⠑⡎⡐⢢⠘⣿⡰⠇⡌⣑⢚⡴⠗⠸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⣀⣴⠟⢫⡔⢛⣾⣿⣻⠟⣡⠊⡜⣀⢣⣿⠖⣍⠧⠔⡠⠁⡞⣻⡟⡰⠜⡐⢊⠔⠨⢥⣇⣀⡀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⢀⣠⡾⢃⡥⢋⠐⢢⢽⣿⡿⢃⠣⡄⡑⠦⣬⢟⡦⡙⡤⢒⠰⣀⠡⢅⠻⣧⢒⡩⢐⢣⠘⠤⠈⣽⣥⢭⡑⣄⠀⠀⠀⠀⠀\n⠀⣴⡟⠤⡑⢎⡐⢤⠿⣛⣿⣟⢧⡃⠒⠠⡑⢊⢦⢏⣷⡱⡑⢊⡔⢢⠜⣂⢣⠙⠦⣁⠃⢆⡉⠲⢏⣿⠠⢌⡟⢊⠣⡀⠀⠀⠀\n⠐⣷⣻⣴⠵⣎⡐⠚⢛⣿⣿⣟⢮⡗⠠⠑⡠⢣⢎⢾⣿⡰⣉⠆⡘⢢⡜⠤⢊⡑⠂⠄⡘⠤⠌⡓⢊⢋⡙⢢⡙⢻⣆⡱⡀⠀⠀\n⠀⢸⣿⣸⣿⣄⢹⣤⣿⣿⣿⣿⣼⣿⣁⠇⣤⢁⡼⢧⣿⢡⠤⡤⢠⣇⢏⡸⢀⠠⢁⠠⢁⡄⣀⠉⠄⠄⡀⢁⠹⡄⢿⣠⡇⠀⠀\n⠀⠀⠹⣿⣵⣚⣻⢾⣿⣿⣿⣿⣿⣿⣿⣽⣲⢮⣝⣯⣟⣯⣟⣞⡯⣔⣺⡔⣣⢜⡠⢆⡡⣄⣴⣩⣼⡰⢠⠁⢠⢡⣻⣧⣻⠀⠀\n⠀⠀⠀⢻⣿⢿⠲⣬⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣿⣾⣿⣿⣿⣿⣽⢶⣿⣞⣿⣾⣿⢧⣿⣿⡱⢧⣏⣿⢤⣿⣷⣿⣦⠀\n⠀⠀⠀⠀⢻⣗⡳⢺⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⣿⣿⣻⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⢯⣾⣿⣯⣿⣧⠇\n⠀⠀⠀⠀⠀⢿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣷⢿⣞⡿⣿⣿⣿⣿⡏⠀\n⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣾⡿⣾⢿⣿⣿⣿⣿⣷⠀\n⠀⠀⠀⠀⠀⠀⠘⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⣿⣻⣯⣿⢿⣿⣻⣿⣿⣟⣾⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠘⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⡽⣿⣻⣿⣿⣿⣿⣿⣽⣿⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣟⣷⢯⣿⡿⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣳⢯⡳⣏⡾⣽⣾⠇⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣽⣟⣷⣿⣞⣷⡽⣽⣿⡟⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣽⣾⣟⡷⣿⢽⣻⣿⣿⡇⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡟⣹⢣⣟⣷⣿⡿⡇⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣷⢯⣞⡴⣣⣿⣿⡟⣯⡽⡟⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣞⣿⣿⣾⣿⡿⢧⣻⢵⡻⠁⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠿⢿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣏⣧⠽⠚⠁⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+        Console.WriteLine("                      /´¯/) \n                    ,/¯  / \n                   /    / \n             /´¯/'   '/´¯¯`·¸ \n          /'/   /    /       /¨¯\\ \n        ('(   ´   ´     ¯~/'   ') \n         \\                 '     / \n          ''   \\           _ ·´ \n            \\              ( \n              \\             \\   ");
     }
 
-    public static void gameMode(int account)
+    public static int gameMode(int account)
     {
         Random random = new Random();
+        
         Console.WriteLine("You entered the game mode.");
+        
         Console.WriteLine("Zvolte sázku.");
-        int bet = Console.Read();
+        int bet = int.Parse(Console.ReadLine());
+        
         Console.WriteLine("Vyberte štastné číslo. 0-9");
-        int happyNumber = Console.Read();
+        int happyNumber = int.Parse(Console.ReadLine());
+        
         Console.WriteLine("Vsadili jste " + bet + " CZK na číslo " + happyNumber);
+        
         int nuber = random.Next(0, 9);
+        Console.WriteLine("Padlo " + nuber);
+        
         if (happyNumber == nuber)
         {
             Console.WriteLine("HURÁ. Vyhrál si " + bet*2);
             account = account + bet*2;
             Console.WriteLine("Zůstatek na vašem účtu je " + account);
         }
+        
+        return account;
     }
 
-    public static void exit(int account)
+    public static int exit(int account)
     {
         Console.WriteLine("Sbohem");
         account = account - 1;
         Console.WriteLine("Zůstatek na vašem účtu je " + account);
+
+        return account;
     }
 
-    public static void userIsIdiot(int account)
+    public static int userIsIdiot(int account)
     {
         Console.WriteLine("Zvolil jste neplatnou možnost. Pokuta 50 CZK");
         account = account - 50;
         Console.WriteLine("Zůstatek na vašem účtu je " + account);
+        
+        return account;
     }
 }
--- test710.cs
+++ test710.cs
@@ -57,7 +57,7 @@ public class test710
         Console.WriteLine("Zadejte celé kladné číslo.");
         int number = Console.Read();
         int numberOfEven = 0;
-        for (int i = 1; i < number; i++)
+        for (int i = 0; i < number-2; i++)
         {
             if (i % 2 == 0)
             {
