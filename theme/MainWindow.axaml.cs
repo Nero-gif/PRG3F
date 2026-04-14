63a3d39fc73c984128f85153ad40153873dadf28
Jakub Lukšík <jakub.luksik@infis.cz>
2026-04-14 13:16:43 +0200

    theme started

---
+++ theme/MainWindow.axaml.cs
@@ -0,0 +1,32 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace theme;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SubmitClick(object? sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void lightTheme(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void darkTheme(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void blueTheme(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
