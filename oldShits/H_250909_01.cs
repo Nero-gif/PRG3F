2fad6f2b9cb68f8c8a6c21296cb1bf8bb0b88a44
Jakub Lukšík <jakub.luksik@infis.cz>
2026-04-14 08:32:01 +0200

    uklid

---
+++ oldShits/H_250909_01.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace PRG3F;
+
+public class H_250909_01
+{
+    public static void Main1(string[] args)
+    {
+        Console.WriteLine("Dyk more, ako se zveš?");
+        string jmeno = Console.ReadLine();
+        Console.WriteLine($"Čusan {jmeno}!");
+        
+        bool lez = !false;
+
+        var tvojemama = 15; //var pozná automaticky typ proměnné
+    }
+}
---
+++ oldShits/H_250916_01.cs
@@ -0,0 +1,13 @@
+﻿namespace PRG3F;
+
+public class H_250916_01
+{
+    public static void Main1(String[] args)
+    {
+        if (int.TryParse(Console.ReadLine(), out int result))
+        {
+            int sum = 0;
+            
+        }
+    }
+}
