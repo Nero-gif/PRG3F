89948b612e6f1ae457a95036973b0614f0513fa8
Jakub Lukšík <jakub.luksik@infis.cz>
2026-05-19 09:18:36 +0200

    basic design and styling

--- CookieClicker/MainWindow.axaml.cs
+++ CookieClicker/MainWindow.axaml.cs
@@ -1,11 +1,43 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace CookieClicker;
 
+public class Variables
+{
+    private Variables() {}
+
+    public static Variables Instance { get; } = new Variables();
+
+    public int CookieCount { get; set; } = 0;
+    public int CpS { get; set; } = 0;
+    public int CpC { get; set; } = 1;
+}
+
+
 public partial class MainWindow : Window
 {
+    
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Settings_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var settingsWindow = new SettingsWindow();
+        settingsWindow.Show();
+    }
+
+    private void Shop_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var shopWindow = new ShopWindow();
+        shopWindow.Show();
+    }
+
+    private void Cookie_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Variables.Instance.CookieCount += Variables.Instance.CpC;
+        CookieCount.Text = Variables.Instance.CookieCount.ToString() + " Cookies";
     }
 }
---
+++ CookieClicker/ShopWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CookieClicker;
+
+public partial class ShopWindow : Window
+{
+    public ShopWindow()
+    {
+        InitializeComponent();
+    }
+}
