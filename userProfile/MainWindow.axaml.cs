7acd036ab3da2054c3bf18aee263159f9d82b826
Jakub Lukšík <jakub.luksik@infis.cz>
2026-06-14 22:46:03 +0200

    language IFs

--- userProfile/MainWindow.axaml.cs
+++ userProfile/MainWindow.axaml.cs
@@ -16,45 +16,19 @@ namespace userProfile
         private void PreviewButton_OnClick(object? sender, RoutedEventArgs e)
         {
             var previewWindow = new PreviewWindow();
-            // var languageComboBox = this.Find<ComboBox>("LanguageComboBox");
-            // var selectedLanguageItem = languageComboBox?.SelectedItem as ComboBoxItem;
-            // string language = selectedLanguageItem?.Content?.ToString() ?? "Nezvoleno";
-            //
-            // string theme = "Nezvoleno";
-            // if (this.Find<RadioButton>("LightThemeRadioButton")?.IsChecked == true)
-            //     theme = "Světlý";
-            // else if (this.Find<RadioButton>("DarkThemeRadioButton")?.IsChecked == true)
-            //     theme = "Tmavý";
-            // else if (this.Find<RadioButton>("BlueThemeRadioButton")?.IsChecked == true)
-            //     theme = "Modrý";
-            //
-            // var selectedOptions = new List<string>();
-            // if (this.Find<CheckBox>("EmailNotificationsCheckBox")?.IsChecked == true)
-            //     selectedOptions.Add("E-mailová upozornění");
-            // if (this.Find<CheckBox>("ShowTipsCheckBox")?.IsChecked == true)
-            //     selectedOptions.Add("Zobrazovat tipy");
-            // if (this.Find<CheckBox>("AutoSaveCheckBox")?.IsChecked == true)
-            //     selectedOptions.Add("Automatické ukládání");
-            //
-            // string options = selectedOptions.Any() ? string.Join("\n", selectedOptions) : "Žádné volby nebyly zapnuty.";
-            //
-            // var previewWindow = new PreviewWindow(language, theme, options);
-            // previewWindow.Show(this);
             
             //jazyk
-            // TODO: tahle věc nefunguje, musí se to předělat, musí se nějak získat obsah vybraného itemu 
-            // previewWindow.LanguageTextBlock.Text = LanguageComboBox.SelectedItem.ToString();
-            // if (LanguageComboBox.SelectedItem.ToString() = "Čeština")
-            // {
-            //     
-            // }else if (LanguageComboBox.SelectedItem.ToString() = "Angličtina")
-            // {
-            //     
-            // }
-            // else
-            // {
-            //     
-            // }
+            if (LanguageComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                previewWindow.LanguageTextBlock.Text = selectedItem.Content.ToString();
+                if (selectedItem.Content.ToString() == "Angličtina")
+                {
+                    
+                } else if (selectedItem.Content.ToString() == "Němčina")
+                {
+                    
+                }
+            }
             
             //motiv
             if (LightThemeRadioButton.IsChecked == true)
