fe9d799c6f175e1c40f7107b2d298b37648a3bb7
Jakub Lukšík <jakub.luksik@infis.cz>
2026-06-15 09:47:52 +0200

    userProfile done

--- userProfile/MainWindow.axaml.cs
+++ userProfile/MainWindow.axaml.cs
@@ -1,7 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using System.Collections.Generic;
-using System.Linq;
+using Avalonia.Media;
 
 namespace userProfile
 {
@@ -10,56 +9,111 @@ namespace userProfile
         public MainWindow()
         {
             InitializeComponent();
-            
+        }
+
+        private void English(PreviewWindow previewWindow)
+        {
+            previewWindow.Header.Text = "Settings Preview";
+            previewWindow.LanguageHeader.Text = "Language";
+            previewWindow.ThemeHeader.Text = "Theme";
+            previewWindow.OptionsHeader.Text = "Options";
+            previewWindow.CloseButton.Content = "Close";
+
+            if (LightThemeRadioButton.IsChecked == true)
+            {
+                previewWindow.ThemeTextBlock.Text = "Light";
+            }
+            else if (BlueThemeRadioButton.IsChecked == true)
+            {
+                previewWindow.ThemeTextBlock.Text = "Blue";
+            }
+            else
+            {
+                previewWindow.ThemeTextBlock.Text = "Dark";
+            }
+
+            //options
+            previewWindow.OptionsTextBlock.Text = "";
+            if (AutoSaveCheckBox.IsChecked == true)
+            {
+                previewWindow.OptionsTextBlock.Text += "Auto-save\n";
+            }
+
+            if (EmailNotificationsCheckBox.IsChecked == true)
+            {
+                previewWindow.OptionsTextBlock.Text += "Email notifications\n";
+            }
+
+            if (ShowTipsCheckBox.IsChecked == true)
+            {
+                previewWindow.OptionsTextBlock.Text += "Show tips\n";
+            }
         }
 
         private void PreviewButton_OnClick(object? sender, RoutedEventArgs e)
         {
             var previewWindow = new PreviewWindow();
-            
+
             //jazyk
-            if (LanguageComboBox.SelectedItem is ComboBoxItem selectedItem)
+            if (LanguageComboBox.SelectedItem is ComboBoxItem { Content: not null } selectedItem)
             {
                 previewWindow.LanguageTextBlock.Text = selectedItem.Content.ToString();
+
                 if (selectedItem.Content.ToString() == "Angličtina")
                 {
-                    
-                } else if (selectedItem.Content.ToString() == "Němčina")
+                    English(previewWindow);
+                }
+                else
                 {
-                    
+                    if (selectedItem.Content.ToString() == "Němčina")
+                    {
+                        previewWindow.LanguageTextBlock.Text =
+                            "Deutsch, německy neumim, takže to překládat nebudu.";
+                    }
+
+                    //motiv
+                    if (LightThemeRadioButton.IsChecked == true)
+                    {
+                        previewWindow.ThemeTextBlock.Text = "Světlý";
+                        previewWindow.Border.Background = Brushes.LightGray;
+                        previewWindow.Header.Foreground = Brushes.Black;
+                        previewWindow.LanguageHeader.Foreground = Brushes.Black;
+                        previewWindow.ThemeHeader.Foreground = Brushes.Black;
+                        previewWindow.OptionsHeader.Foreground = Brushes.Black;
+                        previewWindow.LanguageTextBlock.Foreground = Brushes.Black;
+                        previewWindow.ThemeTextBlock.Foreground = Brushes.Black;
+                        previewWindow.OptionsTextBlock.Foreground = Brushes.Black;
+                    }
+                    else if (BlueThemeRadioButton.IsChecked == true)
+                    {
+                        previewWindow.ThemeTextBlock.Text = "Modrý";
+                        previewWindow.Border.Background = Brushes.DarkBlue;
+                    }
+                    else
+                    {
+                        previewWindow.ThemeTextBlock.Text = "Tmavý";
+                    }
+
+                    //options
+                    previewWindow.OptionsTextBlock.Text = "";
+                    if (AutoSaveCheckBox.IsChecked == true)
+                    {
+                        previewWindow.OptionsTextBlock.Text += "Automatické ukládání\n";
+                    }
+
+                    if (EmailNotificationsCheckBox.IsChecked == true)
+                    {
+                        previewWindow.OptionsTextBlock.Text += "Upozornění na email\n";
+                    }
+
+                    if (ShowTipsCheckBox.IsChecked == true)
+                    {
+                        previewWindow.OptionsTextBlock.Text += "Nabízet tipy\n";
+                    }
                 }
             }
-            
-            //motiv
-            if (LightThemeRadioButton.IsChecked == true)
-            {
-                previewWindow.ThemeTextBlock.Text = "Světlý";
-            } else if (BlueThemeRadioButton.IsChecked == true)
-            {
-                previewWindow.ThemeTextBlock.Text = "Modrý";
-            }
-            else
-            {
-                previewWindow.ThemeTextBlock.Text = "Tmavý";
-            }
-            
-            //options
-            previewWindow.OptionsTextBlock.Text = "";
-            if (AutoSaveCheckBox.IsChecked == true)
-            {
-                previewWindow.OptionsTextBlock.Text += "Automatické ukládání\n";
-            }
 
-            if (EmailNotificationsCheckBox.IsChecked == true)
-            {
-                previewWindow.OptionsTextBlock.Text += "Upozornění na email\n";
-            }
-
-            if (ShowTipsCheckBox.IsChecked == true)
-            {
-                previewWindow.OptionsTextBlock.Text += "Nabízet tipy\n";
-            }
-           previewWindow.Show();
+            previewWindow.Show();
         }
     }
 }
