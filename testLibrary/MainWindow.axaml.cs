a1dc97f07caf87b949c0a31e18220ec2d4c9c76d
Jakub Lukšík <jakub.luksik@infis.cz>
2026-06-16 09:35:34 +0200

    test finished -> 1

--- testLibrary/MainWindow.axaml.cs
+++ testLibrary/MainWindow.axaml.cs
@@ -12,6 +12,46 @@ public partial class MainWindow : Window
 
     private void ShowSummary_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        var summaryWindow = new SummaryWindow();
+
+        if (BookComboBox.SelectedItem is ComboBoxItem {Content: not null } selectedItem)
+        {
+
+            summaryWindow.BookSelected.Text = selectedItem.Content.ToString();
+
+            if (Lenght14.IsChecked ==true)
+            {
+                summaryWindow.Lenght.Text = "14 dní";
+            } else if (Lenght30.IsChecked ==true)
+            {
+                summaryWindow.Lenght.Text = "30 dní";
+            }else if (Lenght60.IsChecked ==true)
+            {
+                summaryWindow.Lenght.Text = "60 dní";
+            }
+
+
+            summaryWindow.Options.Text = "";
+            if (OptionMail.IsChecked == true)
+            {
+                summaryWindow.Options.Text += "Notifikace na email\n";
+            }
+            if (OptionJacket.IsChecked == true)
+            {
+                summaryWindow.Options.Text += "Přebal na knihu\n";
+            }
+            if (OptionExtension.IsChecked == true)
+            {
+                summaryWindow.Options.Text += "Automatické prodloužení\n";
+            }
+            if (OptionConditions.IsChecked == true)
+            {
+                summaryWindow.Options.Text += "Souhlas s podmínkami\n";
+            }
+        }
+        
+        
+        summaryWindow.Show();
+    
     }
 }
---
+++ testLibrary/SummaryWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace testLibrary;
+
+public partial class SummaryWindow : Window
+{
+    public SummaryWindow()
+    {
+        InitializeComponent();
+    }
+}
