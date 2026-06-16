5775af85abbe73ea395227f053a0d44a74dfee3a
Jakub Lukšík <jakub.luksik@infis.cz>
2026-06-16 09:19:57 +0200

    test started

---
+++ testLibrary/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace testLibrary;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ShowSummary_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
